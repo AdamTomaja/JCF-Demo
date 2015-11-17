bd51f960a59d57b658781f7bd758eb3cc37ddfc8
atomaja <atomaja@sollan.com>
2015-11-17 12:27:54 +0100

    MyArrayList javadoc

--- src/pl/tomaja/customimplementation/MyArrayList.java
+++ src/pl/tomaja/customimplementation/MyArrayList.java
@@ -4,6 +4,16 @@ import java.util.AbstractList;
 
 /**
  * This example class shows how to implement Your own collection. In this case unmodifiable List.
+ * 
+ * Other abstract classes we can inherit: 
+ * <ul>
+ *		<li>AbstractCollection</li>
+ *		<li>AbstractSet</li>
+ *		<li>AbstractList</li>
+ *		<li>AbstractSequentialList</li>
+ *		<li>AbstractQueue</li>
+ *		<li>AbstractMap</li>
+ * </ul>
  * @author Adam Tomaja
  */
 public class MyArrayList <T> extends AbstractList<T> {
